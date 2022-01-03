c40c9c508bb88ed6900b09e6e4336d2697c031cc
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-01-03 23:55:48 +0300

    fix: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    if(n > 0){
+        let rn = n.toString().split('').reverse().concat().join('');
+        return Number(rn);
+    } else {
+        let rn = n.toString().split('').slice(1).reverse().concat().join('');
+        return Number(rn);
+    };   
+};
